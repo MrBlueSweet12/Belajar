613cde236621c4d6a0adf57e04647aced78ebfe3
MrBlueSweet12 <mrbluesweet12@gmail.com>
2024-11-19 09:25:15 +0700

    Add files via upload

---
+++ 2024-11-19/pages/register.php
@@ -0,0 +1,23 @@
+<div class="register">
+    <h1>Register</h1>
+    <form action="" method="post">
+        <input type="email" name="email" required placeholder="Masukan Alamat Email" id="">
+        <input type="password" name="password" required placeholder="Masukan Password">
+        <input type="submit" name="register" value="Register">
+    </form>
+</div>
+<?php
+    if (isset($_POST["register"])) {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        // echo $email;
+        // echo "<br>";
+        // echo $password;
+        $sql = "INSERT INTO customer (email, password) VALUES ('$email', '$password')";
+        mysqli_query($koneksi, $sql);
+
+        header("location:index.php?menu=login");
+
+        echo $sql;
+    }
+?>
